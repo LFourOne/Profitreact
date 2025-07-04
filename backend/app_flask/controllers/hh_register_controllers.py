edca7966207e8dc7cce82706880a2979e52172d3
Patricio Torres Jara <patotorres0710@gmail.com>
2025-07-04 17:44:55 -0400

    Cronograma

--- backend/app_flask/controllers/hh_register_controllers.py
+++ backend/app_flask/controllers/hh_register_controllers.py
@@ -6,6 +6,7 @@ from app_flask.models.report_models import Report
 from app_flask.models.hh_models.task_models import Task
 from app_flask.models.hh_models.project_report_models import ProjectReport
 from app_flask.models.hh_models.project_task_models import ProjectTask
+from datetime import datetime, time, timedelta
 
 @app.route('/hh-register')
 def hh_register():
@@ -37,4 +38,46 @@ def get_reports_by_project(project_id):
     return jsonify({
         'reports': project_reports,
         'tasks': project_tasks
+    })
+
+@app.route('/hh-register/process', methods=['POST'])
+def process_hh_register():
+
+    if 'rut_personal' not in session:
+        return jsonify({'status': 'error', 'message': 'Usuario no autorizado'}), 401
+
+    data = request.json
+
+    start_str = data.get('start-time')
+    end_str = data.get('end-time')
+
+    fmt = "%H:%M"
+    start_dt = datetime.strptime(start_str, fmt)
+    end_dt = datetime.strptime(end_str, fmt)
+
+    total_hours = (end_dt - start_dt).total_seconds() / 3600
+
+    lunch_start = datetime.strptime("13:30", fmt)
+    lunch_end = datetime.strptime("14:30", fmt)
+
+    if start_dt < lunch_end and end_dt > lunch_start:
+        total_hours -= 1
+
+    hh_data = {
+        'id_proyecto': data.get('project'),
+        'id_informe': data.get('report'),
+        'id_tarea': data.get('task'),
+        'inicio': data.get('start-time'),
+        'fin': data.get('end-time'),
+        'rut_personal': session['rut_personal'],
+        'id_especialidad': session['id_especialidad'],
+        'fecha': data.get('date'),
+        'horas': round(total_hours, 1)
+    }
+
+    print(hh_data)
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Datos procesados correctamente'
     })
--- frontend/src/pages/HHRegister/HHRegister.jsx
+++ frontend/src/pages/HHRegister/HHRegister.jsx
@@ -7,6 +7,8 @@ import logo from '../../assets/icon1.png'
 
 export function HHRegister() {
 
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
     const navigate = useNavigate();
 
     const [project, setProject] = useState([])
@@ -44,6 +46,28 @@ export function HHRegister() {
     useEffect(() => {
         fetchApi();
     }, []);
+
+    const onSubmit = async (data) => {
+        try {
+            const response = await axios.post('http://localhost:5500/hh-register/process', data, {
+                withCredentials: true,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        } catch (error) {
+            console.error('Error al procesar los datos:', error);
+        }
+        finally {
+            reset();
+            setSelectedProject('');
+            setSelectedReport(null);
+            setSelectedTask('');
+            setReports([]);
+            setTasks([]);
+            console.log('Datos enviados:', data);
+        }
+    }
 
     const handleProjectChange = async (event) => {
             const projectID = event.target.value;
@@ -89,18 +113,18 @@ export function HHRegister() {
                         <img src={logo} className={styles['logo']} alt="Logo de Profit" />
                     </div>
                 </section>
-                <form className={styles['form']}>
+                <form className={styles['form']} onSubmit={handleSubmit(onSubmit)}>
                     <header>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#16a34a" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#16a34a" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                         <h1>Registro de Tarea</h1>
                     </header>
-                    <div className={styles['content']}>
+                    <div className={styles['form-content']}>
                         <div className={styles['content-fields']}>
                             <fieldset>
                                 <label>Proyecto</label>
                                 {
                                     project.length > 0 && (
-                                        <select name="" id="" onChange={handleProjectChange} value={selectedProject}>
+                                        <select name="project" id="project" {...register('project', { required: true })} onChange={handleProjectChange} value={selectedProject}>
                                             {project
                                             .filter((project) => project.id_proyecto != 'JE')
                                             .map((project) => (
@@ -117,7 +141,7 @@ export function HHRegister() {
                                             <label>Informe</label>
                                             {
                                                 reports.length > 0 ? (
-                                                    <select name="" id="" onChange={(e) => {const found = reports.find(report => Number(report.id_informe) === Number(e.target.value)); setSelectedReport(found || null)}} value={selectedReport ? selectedReport.id_informe : ''}>
+                                                    <select name="report" id="report" {...register('report', { required: true })} onChange={(e) => {const found = reports.find(report => Number(report.id_informe) === Number(e.target.value)); setSelectedReport(found || null)}} value={selectedReport ? selectedReport.id_informe : ''}>
                                                         {reports.map((report) => (
                                                             <option key={report.id_informe} value={report.id_informe}>{report.nombre}</option>
                                                         ))}
@@ -133,7 +157,10 @@ export function HHRegister() {
                                             <label>Versión</label>
                                             {
                                                 selectedReport ? (
-                                                    <span className={styles['version']}>{selectedReport.id_version}</span>
+                                                    <>
+                                                        <span className={styles['version']}>{selectedReport.id_version}</span>
+                                                        <input type="hidden" value={selectedReport.id_version} />
+                                                    </>
                                                 )
                                                 :
                                                 (
@@ -147,6 +174,7 @@ export function HHRegister() {
                                                 <select
                                                     name="task"
                                                     id="task"
+                                                    {...register('task', { required: true })}
                                                     value={selectedTask}
                                                     onChange={e => setSelectedTask(e.target.value)}
                                                 >
@@ -187,15 +215,115 @@ export function HHRegister() {
                         <div className={styles['content-fields']}>
                             <fieldset>
                                 <label>Fecha</label>
-                                <input type="date" className={styles['date-time-input']} />
+                                <input type="date" className={styles['date-time-input']} {...register('date', { required: true })} />
                             </fieldset>
                             <fieldset>
                                 <label>Hora Inicio</label>
-                                <input type="time" className={styles['date-time-input']} />
+                                <select className={styles['date-time-input']} defaultValue="" {...register('start-time', { required: true })}>
+                                    <option value="" disabled>Selecciona una opción</option>
+                                    <option>00:00</option>
+                                    <option>00:30</option>
+                                    <option>01:00</option>
+                                    <option>01:30</option>
+                                    <option>02:00</option>
+                                    <option>02:30</option>
+                                    <option>03:00</option>
+                                    <option>03:30</option>
+                                    <option>04:00</option>
+                                    <option>04:30</option>
+                                    <option>05:00</option>
+                                    <option>05:30</option>
+                                    <option>06:00</option>
+                                    <option>06:30</option>
+                                    <option>07:00</option>
+                                    <option>07:30</option>
+                                    <option>08:00</option>
+                                    <option>08:30</option>
+                                    <option>09:00</option>
+                                    <option>09:30</option>
+                                    <option>10:00</option>
+                                    <option>10:30</option>
+                                    <option>11:00</option>
+                                    <option>11:30</option>
+                                    <option>12:00</option>
+                                    <option>12:30</option>
+                                    <option>13:00</option>
+                                    <option>13:30</option>
+                                    <option>14:00</option>
+                                    <option>14:30</option>
+                                    <option>15:00</option>
+                                    <option>15:30</option>
+                                    <option>16:00</option>
+                                    <option>16:30</option>
+                                    <option>17:00</option>
+                                    <option>17:30</option>
+                                    <option>18:00</option>
+                                    <option>18:30</option>
+                                    <option>19:00</option>
+                                    <option>19:30</option>
+                                    <option>20:00</option>
+                                    <option>20:30</option>
+                                    <option>21:00</option>
+                                    <option>21:30</option>
+                                    <option>22:00</option>
+                                    <option>22:30</option>
+                                    <option>23:00</option>
+                                    <option>23:30</option>
+                                </select>
                             </fieldset>
                             <fieldset>
                                 <label>Hora Término</label>
-                                <input type="time" className={styles['date-time-input']} />
+                                <select className={styles['date-time-input']} defaultValue="" {...register('end-time', { required: true })}>
+                                    <option value="" disabled>Selecciona una opción</option>
+                                    <option>00:00</option>
+                                    <option>00:30</option>
+                                    <option>01:00</option>
+                                    <option>01:30</option>
+                                    <option>02:00</option>
+                                    <option>02:30</option>
+                                    <option>03:00</option>
+                                    <option>03:30</option>
+                                    <option>04:00</option>
+                                    <option>04:30</option>
+                                    <option>05:00</option>
+                                    <option>05:30</option>
+                                    <option>06:00</option>
+                                    <option>06:30</option>
+                                    <option>07:00</option>
+                                    <option>07:30</option>
+                                    <option>08:00</option>
+                                    <option>08:30</option>
+                                    <option>09:00</option>
+                                    <option>09:30</option>
+                                    <option>10:00</option>
+                                    <option>10:30</option>
+                                    <option>11:00</option>
+                                    <option>11:30</option>
+                                    <option>12:00</option>
+                                    <option>12:30</option>
+                                    <option>13:00</option>
+                                    <option>13:30</option>
+                                    <option>14:00</option>
+                                    <option>14:30</option>
+                                    <option>15:00</option>
+                                    <option>15:30</option>
+                                    <option>16:00</option>
+                                    <option>16:30</option>
+                                    <option>17:00</option>
+                                    <option>17:30</option>
+                                    <option>18:00</option>
+                                    <option>18:30</option>
+                                    <option>19:00</option>
+                                    <option>19:30</option>
+                                    <option>20:00</option>
+                                    <option>20:30</option>
+                                    <option>21:00</option>
+                                    <option>21:30</option>
+                                    <option>22:00</option>
+                                    <option>22:30</option>
+                                    <option>23:00</option>
+                                    <option>23:30</option>
+                                </select>
                             </fieldset>
                         </div>
                     </div>
@@ -204,11 +332,104 @@ export function HHRegister() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                             Agregar Tarea
                         </button>
-                        <button type="button" className={styles['close-day-button']}>
-                            Cerrar Día
-                        </button>
                     </footer>
                 </form>
+                <section className={styles['schedule-section']}>
+                    <header>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#16a34a" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+                        <h2>Cronograma</h2>
+                    </header>
+                    <div className={styles['schedule-content']}>
+                        <table className={styles['schedule-table']}>
+                            <tbody>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>08:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>09:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>09:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>10:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>10:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>11:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>11:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>12:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>12:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>13:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>13:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>14:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>14:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>15:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>15:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>16:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>16:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>17:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>17:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>18:00</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                                <tr className={styles['schedule-table-tr']}>
+                                    <td>18:30</td>
+                                    <td className={styles['td-hh']}></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
             </main>
         )}
         </>
